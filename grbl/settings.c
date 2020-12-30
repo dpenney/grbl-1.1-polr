c3b67aef1edf8ec07a64d72f2a803ba937c950e6
Damian Penney <damian@penney.org>
2020-12-30 12:29:21 -0800

    Set distance between steppers via $28

--- grbl/settings.c
+++ grbl/settings.c
@@ -284,6 +284,7 @@ uint8_t settings_store_global_setting(uint8_t parameter, float value) {
       case 25: settings.homing_seek_rate = value; break;
       case 26: settings.homing_debounce_delay = int_value; break;
       case 27: settings.homing_pulloff = value; break;
+      case 28: settings.distance = value; break;
       case 30: settings.rpm_max = value; spindle_init(); break; // Re-initialize spindle rpm calibration
       case 31: settings.rpm_min = value; spindle_init(); break; // Re-initialize spindle rpm calibration
       case 32:
